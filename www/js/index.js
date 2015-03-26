02fd0b7dbb8fb7cbfa394d7cbbd2d90f11e48ea2
CooCooKerChoo <matt_meadows@live.co.uk>
2015-03-26 22:17:01 +0000

    revamped geolocation code in index.js

--- www/js/index.js
+++ www/js/index.js
@@ -24,18 +24,22 @@
 //     });
 // }
 
-function DOMLoaded() {
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
 
-    document.addEventListener("deviceready", phonegapLoaded, false);
-
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
 }
 
-function phonegapLoaded() {
-    navigator.geolocation.getCurrentPosition(onSuccess);
-}
-
-function onSuccess(position) {
-
-    alert("Latitude: " + position.coords.latitude + "\n" +
-        "Timestamp: " + new Date(position.timestamp));
-}
+navigator.geolocation.getCurrentPosition(onSuccess, onError);

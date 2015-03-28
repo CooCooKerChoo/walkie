39a9e00e9e7db947e568145f0c2efad824b243bc
CooCooKerChoo <matt_meadows@live.co.uk>
2015-03-28 16:41:55 +0000

    added phonegape example watch code

--- www/js/main.js
+++ www/js/main.js
@@ -59,6 +59,8 @@
 
   // START OF LOCATION TRACK //
 
+    document.addEventListener("deviceready", onDeviceReady, false);
+
     var watchID = null;
 
     // device APIs are available
@@ -68,30 +70,29 @@
         // device.
         var options = { enableHighAccuracy: true };
         watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
-        
-        // onSuccess Geolocation
-        //
-        function onSuccess(position) {
-            var element = document.getElementById('geolocation');
-            element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-                                'Longitude: ' + position.coords.longitude     + '<br />' +
-                                '<hr />'      + element.innerHTML;
-        }
-
-        // clear the watch that was started earlier
-        //
-        function clearWatch() {
-            if (watchID != null) {
-                navigator.geolocation.clearWatch(watchID);
-                watchID = null;
-            }
-        }
-
-            // onError Callback receives a PositionError object
-            //
-            function onError(error) {
-              alert('code: '    + error.code    + '\n' +
-                    'message: ' + error.message + '\n');
-            }
     }
 
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />' +
+                            '<hr />'      + element.innerHTML;
+    }
+
+    // clear the watch that was started earlier
+    //
+    function clearWatch() {
+        if (watchID != null) {
+            navigator.geolocation.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+          alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }

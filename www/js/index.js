4cf9cc2c906499a9a699817017d2aa35cd58d206
CooCooKerChoo <matt_meadows@live.co.uk>
2015-03-28 15:50:04 +0000

    added watch location to button click

--- www/js/index.js
+++ www/js/index.js
@@ -25,41 +25,57 @@
 // }
 
 
-var onSuccess = function(position) {
-    // alert('Latitude: '          + position.coords.latitude          + '\n' +
-    //       'Longitude: '         + position.coords.longitude         + '\n' +
-    //       'Timestamp: '         + position.timestamp                + '\n');
+// var onSuccess = function(position) {
 
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var coords = new google.maps.LatLng(latitude, longitude);
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     var coords = new google.maps.LatLng(latitude, longitude);
 
-    var mapOptions = {
-        zoom: 17,
-        center: coords,
-        mapTypeControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+//     var mapOptions = {
+//         zoom: 17,
+//         center: coords,
+//         mapTypeControl: true,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
 
-    //create the map, and place it in the HTML map div
-    map = new google.maps.Map(
-        document.getElementById("mapPlaceholder"), mapOptions
-        );
+//     //create the map, and place it in the HTML map div
+//     map = new google.maps.Map(
+//         document.getElementById("mapPlaceholder"), mapOptions
+//         );
 
-    //place the initial marker
-    var marker = new google.maps.Marker({
-        position: coords,
-        map: map,
-        title: "Current location!"
-    });
+//     //place the initial marker
+//     var marker = new google.maps.Marker({
+//         position: coords,
+//         map: map,
+//         title: "Current location!"
+//     });
 
-};
+// };
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+// // onError Callback receives a PositionError object
+// //
+// function onError(error) {
+//     alert('code: '    + error.code    + '\n' +
+//           'message: ' + error.message + '\n');
+// }
+
+// navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+function trackLocation() {
+
+    var options = { timeout: 30000 };
+
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
 }
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+function onSuccess() {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />'      + element.innerHTML;
+}
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }

d49dcfdceba2ce39dec66b7e765562c60cf419eb
CooCooKerChoo <matt_meadows@live.co.uk>
2015-03-28 17:03:58 +0000

    readded get current position on deviceready

--- www/js/main.js
+++ www/js/main.js
@@ -1,60 +1,65 @@
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    // function showCurrentLocation(position)
-    //  {
-    //     var latitude = position.coords.latitude;
-    //     var longitude = position.coords.longitude;
-    //     var coords = new google.maps.LatLng(latitude, longitude);
-
-    //     var mapOptions = {
-    //         zoom: 17,
-    //         center: coords,
-    //         mapTypeControl: true,
-    //         mapTypeId: google.maps.MapTypeId.ROADMAP
-    //     };
-
-    //     //create the map, and place it in the HTML map div
-    //     map = new google.maps.Map(
-    //         document.getElementById("mapPlaceholder"), mapOptions
-    //         );
-
-    //     //place the initial marker
-    //     var marker = new google.maps.Marker({
-    //         position: coords,
-    //         map: map,
-    //         title: "Current location!"
-    //     });
-    // }
-
-
-    // var onSuccess = function(position) {
-
-    //     var latitude = position.coords.latitude;
-    //     var longitude = position.coords.longitude;
-    //     var coords = new google.maps.LatLng(latitude, longitude);
-
-    //     var mapOptions = {
-    //         zoom: 17,
-    //         center: coords,
-    //         mapTypeControl: true,
-    //         mapTypeId: google.maps.MapTypeId.ROADMAP
-    //     };
-
-    //     //create the map, and place it in the HTML map div
-    //     map = new google.maps.Map(
-    //         document.getElementById("mapPlaceholder"), mapOptions
-    //         );
-
-    //     //place the initial marker
-    //     var marker = new google.maps.Marker({
-    //         position: coords,
-    //         map: map,
-    //         title: "Current location!"
-    //     });
-
-    // };
-
-    // onError Callback receives a PositionError object
+    // device APIs are available
     //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    function showCurrentLocation(position)
+     {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var coords = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+            zoom: 17,
+            center: coords,
+            mapTypeControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        //create the map, and place it in the HTML map div
+        map = new google.maps.Map(
+            document.getElementById("mapPlaceholder"), mapOptions
+            );
+
+        //place the initial marker
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            title: "Current location!"
+        });
+    }
+
+
+    var onSuccess = function(position) {
+
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var coords = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+            zoom: 17,
+            center: coords,
+            mapTypeControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        //create the map, and place it in the HTML map div
+        map = new google.maps.Map(
+            document.getElementById("mapPlaceholder"), mapOptions
+            );
+
+        //place the initial marker
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            title: "Current location!"
+        });
+
+    };
+    
 
 
   // START OF LOCATION TRACK //

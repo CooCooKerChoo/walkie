fe390f984f0899de5774c47da98d35ef23f48132
CooCooKerChoo <matt_meadows@live.co.uk>
2015-03-31 02:42:06 +0100

    fixed but with embedded js at footer

--- www/js/main.js
+++ www/js/main.js
@@ -5,7 +5,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
         var posOptions = { enableHighAccuracy: true, timeout : 10000, maximumAge: 60000};
         navigator.geolocation.getCurrentPosition(onSuccess, onError, posOptions);
-
     }
 
 

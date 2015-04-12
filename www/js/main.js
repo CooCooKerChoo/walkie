2af3c016201a256a54468bd799140303ad6e0dab
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-12 18:45:38 +0100

    moved database storage back into img.onload function for camera api

--- www/js/main.js
+++ www/js/main.js
@@ -373,10 +373,11 @@ function addMarkerFail(error) {
             context.drawImage( img, 0, 0 );
             canvas.style.width = "100%"; 
             data = canvas.toDataURL("image/jpeg");
-        }
+
             db.transaction(function(t) {
                 t.executeSql('INSERT INTO WALKS(Images) values (?)' , [data]);
             });
+        }
     }
 
     function onCameraFail(message) {

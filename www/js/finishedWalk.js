a18e9f05ffd423e606cf0318be7fe83789b619cb
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-13 04:18:49 +0100

    corrected error with popout creation

--- www/js/finishedWalk.js
+++ www/js/finishedWalk.js
@@ -79,7 +79,7 @@
 
                     $(".photos").append('<a href="#dialog' + i + '"data-rel="popup"><img class="image" src="' + imageArray[i] + '"></a>');
 
-                    $("#map-page").append('<div data-role="popup" class="imagePopups" id="#popup' + i + '><a hef="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a><img class="popupphoto" src="' + imageArray[i] + '" width="300"></div>').trigger('create');
+                    $("#map-page").append('<div data-role="popup" class="imagePopups" id="#popup' + i + '><a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a><img class="popupphoto" src="' + imageArray[i] + '" width="300"></div>').trigger('create');
 
                     $(".imagePopups").popup();
                 }

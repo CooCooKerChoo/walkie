9af5448ec053597721b51e93b39102e083a29eb2
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-16 20:47:47 +0100

    removed web sql script from finishedWalk page to check if notifications work

--- www/js/finishedWalk.js
+++ www/js/finishedWalk.js
@@ -73,26 +73,26 @@
                 var markersArray = JSON.stringify(markers);
                 var walkID; 
 
-                db.transaction(function(t) {
-                    t.executeSql('INSERT INTO WALKS (Duration, Distance, PathCoordinates, Images, WalkTitle, WalkDescription) values (?,?,?,?,?,?)', [dbstoreDuration, dbstoreDistance, googleLatLng, imageArray, walkTitle, walkDescription], 
-                        function(t, results){
-                            console.log('ok');
-                            walkID = results.insertId;
-                            for(var id in markers) {
-                                if( markers.hasOwnProperty(id) ) {
-                                    (function(id) {
-                                        db.transaction(function(t) {
-                                            var marker = markers[id];
-                                            t.executeSql('INSERT INTO MARKERS (markerid, title, info, markerLat, markerLng, walk_id) values (?,?,?,?,?,?)', [markers[id].id, markers[id].title, markers[id].info, markers[id].lat, markers[id].lng, walkID]);
-                                        });
-                                        })(id);
-                                }
-                            }
-                        },
-                        function(t, error){
-                            console.log('error: ' + error.meesage);
-                        });
-                });
+                // db.transaction(function(t) {
+                //     t.executeSql('INSERT INTO WALKS (Duration, Distance, PathCoordinates, Images, WalkTitle, WalkDescription) values (?,?,?,?,?,?)', [dbstoreDuration, dbstoreDistance, googleLatLng, imageArray, walkTitle, walkDescription], 
+                //         function(t, results){
+                //             console.log('ok');
+                //             walkID = results.insertId;
+                //             for(var id in markers) {
+                //                 if( markers.hasOwnProperty(id) ) {
+                //                     (function(id) {
+                //                         db.transaction(function(t) {
+                //                             var marker = markers[id];
+                //                             t.executeSql('INSERT INTO MARKERS (markerid, title, info, markerLat, markerLng, walk_id) values (?,?,?,?,?,?)', [markers[id].id, markers[id].title, markers[id].info, markers[id].lat, markers[id].lng, walkID]);
+                //                         });
+                //                         })(id);
+                //                 }
+                //             }
+                //         },
+                //         function(t, error){
+                //             console.log('error: ' + error.meesage);
+                //         });
+                // });
 
                 navigator.notification.alert(
                     'All information has been saved',  // message

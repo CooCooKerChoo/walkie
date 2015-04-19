42484d86b2ecb2a44bde7dc097e01061993258c5
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-19 20:53:44 +0100

    added map output code

--- www/js/myroutes.js
+++ www/js/myroutes.js
@@ -16,15 +16,15 @@
 
 	    for (var i=0; i<len; i++){
 	    	var polylinePath = [];
-	    	walkId = results.rows.item(i).id;
-	    	walkTitle = results.rows.item(i).WalkTitle;
-	    	walkDistance = results.rows.item(i).Distance;
-	    	walkDuration = results.rows.item(i).Duration;
-		    path = results.rows.item(i).PathCoordinates;
-			path=path.substr(1,path.length-1);	path=path.substr(0,path.length-1); 
-			polyline = path.split("),("); 
+	    	var walkId = results.rows.item(i).id;
+	    	var walkTitle = results.rows.item(i).WalkTitle;
+	    	var walkDistance = results.rows.item(i).Distance;
+	    	var walkDuration = results.rows.item(i).Duration;
+		    var path = results.rows.item(i).PathCoordinates;
+			var path=path.substr(1,path.length-1);	path=path.substr(0,path.length-1); 
+			var polyline = path.split("),("); 
 			var sumLat=0, sumLng=0, maxLat, minLat, maxLng, minLng; 
-			polyline=polyline.map(function(p){ 
+			var polyline=polyline.map(function(p){ 
 				var point=p.split(','); 
 				// var lat=(Math.random()/100) ,lng=(Math.random()/100); 
 				var lat=parseFloat(point[0]) ,lng=parseFloat(point[1]); 
@@ -98,6 +98,73 @@ $(document).on('pageshow', "#route_details", function() {
 
 
  	function querySuccessDetails(t, results) {
+var coords = [];
+		var polys = [];
+
+
+	    for (var i=0; i<len; i++){
+	    	var polylinePath = [];
+	    	var walkId = results.rows.item(i).id;
+	    	var walkTitle = results.rows.item(i).WalkTitle;
+	    	var walkDistance = results.rows.item(i).Distance;
+	    	var walkDuration = results.rows.item(i).Duration;
+		    var path = results.rows.item(i).PathCoordinates;
+			var path=path.substr(1,path.length-1);	path=path.substr(0,path.length-1); 
+			var polyline = path.split("),("); 
+			var sumLat=0, sumLng=0, maxLat, minLat, maxLng, minLng; 
+			var polyline=polyline.map(function(p){ 
+				var point=p.split(','); 
+				// var lat=(Math.random()/100) ,lng=(Math.random()/100); 
+				var lat=parseFloat(point[0]) ,lng=parseFloat(point[1]); 
+				if(maxLat===undefined || maxLat<lat) maxLat=lat;
+				if(maxLng===undefined || maxLng<lng) maxLng=lng;
+				if(minLat===undefined || minLat>lat) minLat=lat;
+				if(minLng===undefined || minLng>lng) minLng=lng;
+				sumLat+=lat; sumLng+=lng; 
+				return new google.maps.LatLng(lat,lng); 
+			});
+			coords.push([sumLat/polyline.length, sumLng/polyline.length, maxLat, minLat, maxLng, minLng]);
+			polys.push(polyline);
+	    }
+	    
+		$('.map').each(function (index, Element) {
+		    // var latlng = new google.maps.LatLng(parseFloat(coords[0]), parseFloat(coords[1]));
+		    var myOptions = {
+		        zoom: 14,
+		        center: new google.maps.LatLng(coords[index][0],coords[index][1]),
+		        // center: new google.maps.LatLng(52.9544124,-2.0046446),
+		        mapTypeId: google.maps.MapTypeId.TERRAIN,
+		        disableDefaultUI: false,
+		        mapTypeControl: true,
+            	mapTypeControl: false,
+                streetViewControl:false,
+                zoomControl: false,
+                draggable: false,
+		        zoomControlOptions: {
+		            style: google.maps.ZoomControlStyle.SMALL
+		        }
+		    };
+
+		    var map = new google.maps.Map(Element, myOptions);
+
+
+		    var sw = new google.maps.LatLng(coords[index][3],coords[index][5]);
+		    var ne = new google.maps.LatLng(coords[index][2],coords[index][4]);
+			map.fitBounds(new google.maps.LatLngBounds(sw,ne));
+
+	        setTimeout(function() {
+	            google.maps.event.trigger(map, "resize");
+	        }, 1000);
+
+              var path = new google.maps.Polyline({
+                path: polys[index],
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 5
+              });
+              path.setMap(map);
+		});
+
 		var len = results.rows.length;
 		console.log("Walks table: " + len + " rows found");
     	walkTitle = results.rows.item(0).WalkTitle;

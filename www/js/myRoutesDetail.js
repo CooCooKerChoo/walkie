85eaa14d240f54066e9d77f10ac0c5264ca36e1f
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-21 17:49:09 +0100

    readded array variables for image

--- www/js/myRoutesDetail.js
+++ www/js/myRoutesDetail.js
@@ -33,6 +33,9 @@ $(document).on('pagebeforeshow', "#route_details", function() {
 		Routecoords.push([sumLat/polyline.length, sumLng/polyline.length, maxLat, minLat, maxLng, minLng]);
 		Routepolys.push(polyline);
 
+	    Routephotos = results.rows.item(0).Images;
+	    routePhotosArray = Routephotos.split(",");
+
 	    for( var i = 0, c = routePhotosArray.length; i < c; i++ ) {
 	        $(".photos").append('<a href="#Imagepopup' + i + '"data-rel="popup" data-position-to="window" data-transition="fade"><img class="image" src="' + routePhotosArray[i] + '"></a>');
 	        $('#route_details').append('<div data-role="popup" id="Imagepopup' + i + '" class="imagePopups" data-overlay-theme="a" data-theme="d" data-corners="false"><a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a> <img class="popphoto" src="' + routePhotosArray[i] + '" style="max-height:512px;" alt="photo, test"></div>').trigger('create');

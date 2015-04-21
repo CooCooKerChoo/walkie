773f0cb57ea271f91d82efbd3085bfa5f4c1d6bc
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-21 17:43:23 +0100

    moved photos array output into querySuccessImages function

--- www/js/myRoutesDetail.js
+++ www/js/myRoutesDetail.js
@@ -33,7 +33,13 @@ $(document).on('pagebeforeshow', "#route_details", function() {
 		Routecoords.push([sumLat/polyline.length, sumLng/polyline.length, maxLat, minLat, maxLng, minLng]);
 		Routepolys.push(polyline);
 
+		$('#headerWalkTitle').html(walkTitle);
+		$('#walkTitleDetails').val(walkTitle);
+		$('#walkDescriptionDetails').val(walkDescription);
+		$('#finalDistanceDetails').html(walkDistance);
+		$('#finalDurationDetails').html(walkDuration);
 	}
+
 	function errorCBDetails(error) {
 		console.log("Error processing SQL: " + error.message);
 	}
@@ -53,18 +59,12 @@ $(document).on('pageshow', "#route_details", function() {
  	function querySuccessImages(t, results) {
 	    Routephotos = results.rows.item(0).Images;
 	    routePhotosArray = Routephotos.split(",");
-	}
 
-    for( var i = 0, c = routePhotosArray.length; i < c; i++ ) {
-        $(".photos").append('<a href="#Imagepopup' + i + '"data-rel="popup" data-position-to="window" data-transition="fade"><img class="image" src="' + routePhotosArray[i] + '"></a>');
-        $('#route_details').append('<div data-role="popup" id="Imagepopup' + i + '" class="imagePopups" data-overlay-theme="a" data-theme="d" data-corners="false"><a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a> <img class="popphoto" src="' + routePhotosArray[i] + '" style="max-height:512px;" alt="photo, test"></div>').trigger('create');
+	    for( var i = 0, c = routePhotosArray.length; i < c; i++ ) {
+	        $(".photos").append('<a href="#Imagepopup' + i + '"data-rel="popup" data-position-to="window" data-transition="fade"><img class="image" src="' + routePhotosArray[i] + '"></a>');
+	        $('#route_details').append('<div data-role="popup" id="Imagepopup' + i + '" class="imagePopups" data-overlay-theme="a" data-theme="d" data-corners="false"><a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a> <img class="popphoto" src="' + routePhotosArray[i] + '" style="max-height:512px;" alt="photo, test"></div>').trigger('create');
+		}
 	}
-
-	$('#headerWalkTitle').html(walkTitle);
-	$('#walkTitleDetails').val(walkTitle);
-	$('#walkDescriptionDetails').val(walkDescription);
-	$('#finalDistanceDetails').html(walkDistance);
-	$('#finalDurationDetails').html(walkDuration);
 
 	$('#routemap').each(function (index, Element) {
 		    // var latlng = new google.maps.LatLng(parseFloat(coords[0]), parseFloat(coords[1]));

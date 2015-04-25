3e7c24d0eb84e925bf21878f830df583a5ccc73c
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-26 00:04:56 +0100

    set bounds variables to be global

--- www/js/myroutes.js
+++ www/js/myroutes.js
@@ -51,9 +51,9 @@ $(document).on('pageinit', "#my-routes", function() {
 		    };
 
 
-		    var sw = new google.maps.LatLng(Routecoords[index][3],Routecoords[index][5]);
-		    var ne = new google.maps.LatLng(Routecoords[index][2],Routecoords[index][4]);
-		    
+		    sw = new google.maps.LatLng(Routecoords[index][3],Routecoords[index][5]);
+		    ne = new google.maps.LatLng(Routecoords[index][2],Routecoords[index][4]);
+
 		    allMyRoutesMap = new google.maps.Map(Element, myOptions);
 
               var path = new google.maps.Polyline({

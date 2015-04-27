58f8e1b6a6b3d65b9a0bb4f2d069d5ad9127788c
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-27 05:38:46 +0100

    added error reporting for ajax get and also changed output container name

--- www/allroutes.js
+++ www/allroutes.js
@@ -4,7 +4,10 @@ $.ajax({    //create an ajax request to load_page.php
         url: "http://matt-meadows.co.uk/walkie/ajaxGET.php",             
         dataType: "html",   //expect html to be returned                
         success: function(response){                    
-            $("#responsecontainer").html(response); 
+            $("#all_walks").html(response); 
             //alert(response);
+        },
+        error: function(error){
+        	alert(error);
         }
 }

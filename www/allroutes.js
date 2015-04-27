5c43ffbd31f03bdbb1b98b4a8fa0d341a0cf14bd
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-27 05:47:29 +0100

    changed get ajax function to expect json instead of html

--- www/allroutes.js
+++ www/allroutes.js
@@ -2,10 +2,9 @@ $(document).on('pageinit', "#my-routes", function() {
 $.ajax({    //create an ajax request to load_page.php
         type: "GET",
         url: "http://matt-meadows.co.uk/walkie/ajaxGET.php",             
-        dataType: "html",   //expect html to be returned                
+        dataType: "json",   //expect html to be returned                
         success: function(response){                    
-            $("#all_walks").html(response); 
-            alert(response);
+            console.log(response);
         },
         error: function(error){
         	alert(error);

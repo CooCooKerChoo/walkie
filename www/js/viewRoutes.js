44dbd062138098f087cd70609e2e61c623e6446b
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-27 22:55:13 +0100

    attempting to output all database data

--- www/js/viewRoutes.js
+++ www/js/viewRoutes.js
@@ -16,16 +16,8 @@ $(document).on('pageshow', "#view_routes", function() {
       url: 'http://matt-meadows.co.uk/walkie/ajaxGET.php',                  //the script to call to get data          
       data: "",                        //you can insert url argumnets here to pass to api.php
       dataType: 'json',                //data format      
-      success: function(data, response)          //on recieve of reply
+      success: function(data)          //on recieve of reply
       {
-        var id = data[0];              //get id
-        var vname = data[1];           //get name
-        //--------------------------------------------------------------------
-        // 3) Update html content
-        //--------------------------------------------------------------------
-        $('#all_walks').html("<b>id: </b>"+id+"<b> name: </b>"+vname); //Set output element html
-        //recommend reading up on jquery selectors they are awesome 
-        // http://api.jquery.com/category/selectors/
         console.log(data);
       }, 
         error: function(jqXHR, textStatus, errorThrown){

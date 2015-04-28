7feeb83515a2938242c1b38bb6931b74e7b27ffe
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-28 03:57:47 +0100

    attempting to immediately show fb info on my routes page

--- www/js/myroutes.js
+++ www/js/myroutes.js
@@ -77,25 +77,16 @@ $(document).on("click", ".walkPage", function(){
 });
 
  $(document).on('pageshow', "#my-routes", function() {
- 	function checkFBLogin(){
-		openFB.login(
-			function(response) {
-		        if(response.status === 'connected') {
-		        	showFBInfo();
-		        }
-		)
- 	}
-
-	function showFBInfo(){
-		openFB.api({
-		    path: '/me',
-		    success: function(data) {
-		        console.log(JSON.stringify(data));
-		        document.getElementById("userName").innerHTML = data.name;
-		        document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
-		    },
-		    error: errorHandler});
-	}
+    function getInfo() {
+        openFB.api({
+            path: '/me',
+            success: function(data) {
+                console.log(JSON.stringify(data));
+                document.getElementById("userName").innerHTML = data.name;
+                document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
+            },
+            error: errorHandler});
+    }
 
 		$('.map').each(function (index, Element) {
 

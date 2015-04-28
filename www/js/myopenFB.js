18e0486bc47ea73f756150fe04e79badb0d4927c
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-28 01:10:32 +0100

    added all openfb functions to separate js file and linked to every page

---
+++ www/js/myopenFB.js
@@ -0,0 +1,41 @@
+	 openFB.init({appId: '292811390842470', tokenStore: window.localStorage});
+
+	function login() {
+	    openFB.login(
+	            function(response) {
+	                if(response.status === 'connected') {
+	                    alert('Facebook login succeeded, got access token: ' + response.authResponse.token);
+	                } else {
+	                    alert('Facebook login failed: ' + response.error);
+	                }
+	            }, {scope: 'email,read_stream,publish_stream'});
+	    getInfo();
+	}
+
+    function getInfo() {
+        openFB.api({
+            path: '/me',
+            success: function(data) {
+                console.log(JSON.stringify(data));
+                document.getElementById("userName").innerHTML = data.name;
+                document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
+            },
+            error: errorHandler});
+    }
+
+    function share() {
+        openFB.api({
+            method: 'POST',
+            path: '/me/feed',
+            params: {
+                message: 'http://matt-meadows.co.uk/walkie/route/'+clicked_route
+            },
+            success: function() {
+                alert('the item was posted on Facebook');
+            },
+            error: errorHandler});
+    }
+
+    function errorHandler(error) {
+        alert(error.message);
+    }

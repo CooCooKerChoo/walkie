a34083bd8ee3b6eedfa700be821ffe907c11b22d
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-29 19:24:09 +0100

    added console log for timer as proof of testing

--- www/js/main.js
+++ www/js/main.js
@@ -337,6 +337,7 @@ function toDate( totalSec ) {
         seconds = totalSec % 60;
 
     return (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+    console.log(hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
 }
 
 

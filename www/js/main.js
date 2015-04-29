e55a79b6ffa3ca330ae84476a16abb7353ee419d
CooCooKerChoo <matt_meadows@live.co.uk>
2015-04-29 18:20:47 +0100

    added error code handling for get position

--- www/js/main.js
+++ www/js/main.js
@@ -99,8 +99,20 @@ var googleLatLng = [],
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.")
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.")
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.")
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.")
+                break;
+        }
     }
 // =================================================== START OF CUSTOM MARKERS ================================================== //
 
